05cebff43e189875bbc34d0c7d8a644b9f0cffa8
EnniBelly <115544985+EnniBelly@users.noreply.github.com>
2022-11-01 18:17:16 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,17 @@
+const searchBtn = document.querySelector('.search-btn');
+const cancelBtn = document.querySelector('.cancel-btn');
+const searchBox = document.querySelector('.search');
+const searchInput = document.querySelector('.search-input');
+
+searchBtn.onclick = () => {
+  searchBox.classList.add('active');
+  searchInput.classList.add('active');
+  searchBtn.classList.add('active');
+  cancelBtn.classList.add('active');
+}
+cancelBtn.onclick = () => {
+  searchBox.classList.remove('active');
+  searchInput.classList.remove('active');
+  searchBtn.classList.remove('active');
+  cancelBtn.classList.remove('active');
+}
